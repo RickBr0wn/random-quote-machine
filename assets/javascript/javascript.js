012cf96da4e949796d767759950bb0e38cd99f76
RiCK BROWN <ricky.brown.00@gmail.com>
2018-02-18 15:43:17 +0000

    Update fonts

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,6 @@
 let url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
 
-let colors = ["#A7BED3", "#C6E2E9", "#F1FFC4", "#FFCAAF", "#DAB894", "#DCEDB9", "#D259E", "#CBD081", "#918868", "#3A405A", "#F9DEC9", "#99B2DD", "#E9AFA3", "#685044", "#474A2C", "#636940", "#59A96A", "#B4E7CE", "#A0ECD0", "##94DDBC", "714955", "#B4E7CE"];
+let colors = ["#A7BED3", "#C6E2E9", "#F1FFC4", "#FFCAAF", "#DAB894", "#DCEDB9", "#D259EB", "#CBD081", "#918868", "#3A405A", "#F9DEC9", "#99B2DD", "#E9AFA3", "#685044", "#474A2C", "#636940", "#59A96A", "#B4E7CE", "#A0ECD0", "#94DDBC", "#714955", "#B4E7CE"];
 
 let quoteText;
 let quoteAuthor;
@@ -14,13 +14,26 @@ let settings = {
     "method": "GET"
 }
 
+$(document).ready(function(){
+    changeColor();
+});
+
 function changeColor(){
     $.getJSON(settings, function(response, success) {
         quoteText = response.quoteText;
-        quoteAuthor = response.quoteAuthor;
+        quoteAuthor = '- ' + response.quoteAuthor;
         
         let random = (Math.floor(Math.random(colors.length - 1)*colors.length));
+        
+        // if(colors[random] == "#DCEDB9" || colors[random] == "#FFCAAF" ||  colors[random] == "#C6E2E9" || colors[random] == "#F1FFC4" || colors[random] == "#F9DEC9"){
+        //     console.log("yes!!");
+        //     quote.style.color = "#000";
+        // }else{
+        //     quote.style.color = "#fff";
+        // }
+
         document.body.style.backgroundColor = colors[random];
+        console.log(colors[random]);
 
         quote.innerHTML = quoteText;
         author.innerHTML = quoteAuthor;
